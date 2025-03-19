f13aacd37fc55911b84639d71f3fafd467470c4e
Melany-JGM <melany28558j@gmail.com>
2025-03-18 20:45:16 -0600

    danza

--- eventos.php
+++ eventos.php
@@ -132,7 +132,7 @@
   <!-- 1era fila de imágenes -->
   <div class="row">
     <div class="col-md-4 zoomp">
-      <img src="public/image/eventos/50 ani.jpg" height="300px" width="200%" alt="50 Aniversario Primaria Jorge L Tamayo" data-bs-toggle="modal" data-bs-target="#imageModal">
+      <img src="public/image/eventos/50 ani.jpg" height="300px" width="300%" alt="50 Aniversario Primaria Jorge L Tamayo" data-bs-toggle="modal" data-bs-target="#imageModal">
       <br><br>
     </div>
 
@@ -199,7 +199,7 @@ De la cual, se obtuvieron las siguientes posiciones:
  <section id="galeria">
     <div class="row">
       <div class="col-md-4 zoomp">
-        <img src="public/image/eventos/atle.jpg" height="350px" width="200%" 
+        <img src="public/image/eventos/atle.jpg" height="350px" width="300%" 
              alt="Eventos deportivos de Atletismo" data-bs-toggle="modal" data-bs-target="#modalAtletismo">
         <br><br>
       </div>
@@ -265,6 +265,134 @@ De la cual, se obtuvieron las siguientes posiciones:
   <!-- 1era fila de imágenes -->
   <div class="row">
     <div class="col-md-4 zoomp">
+      <img src="public/image/eventos/escol.jpg" height="300px" width="300%" alt="Concurso de escoltas" data-bs-toggle="modal" data-bs-target="#modalEscolta">
+      <br><br>
+    </div>
+
+   <!-- Modal con Carrusel -->
+<div class="modal fade" id="modalEscolta" tabindex="-1" aria-labelledby="modalEscoltaLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalEscoltaLabel">Concurso de escoltas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+      </div>
+      <div class="modal-body">
+        <!-- Carrusel -->
+        <div id="carouselEscolta" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="public/image/eventos/escol1.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 1">
+            </div>
+            <div class="carousel-item">
+              <img src="public/image/eventos/escol2.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 2">
+            </div>
+            <div class="carousel-item">
+              <img src="public/image/eventos/escol3.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 3">
+            </div>
+            <div class="carousel-item">
+              <img src="public/image/eventos/escol4.jpg" class="d-block mx-auto" style="width: 30%; height: auto;" alt="Imagen 4">
+            </div>
+            <div class="carousel-item">
+              <img src="public/image/eventos/escol5.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 5">
+            </div>
+          </div>
+          <!-- Controles del Carrusel -->
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselEscolta" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselEscolta" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Siguiente</span>
+          </button>
+        </div>
+        <p class="mt-3 text-center">Nos vamos con la satisfacción de que se hizo el esfuerzo para alcanzar la meta, y que la hermandad entre las diferentes escuelas se refrenda en este tipo de eventos.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+ <!-- Sección de Eventos club de danza  -->
+  </div>
+      <div class="col-md-6">
+      <h2><i class="bi bi-gear me-2"></i>Club de danza </h2>
+      <p style="text-align: justify;">Participación del club de danza y alumnos de nuestra institución, en la presentación de los cuadros de baile "Veracruz" y danzón.</p>
+      
+ <!-- GALERÍA -->
+ <section id="galeria">
+    <div class="row">
+      <div class="col-md-4 zoomp">
+        <img src="public/image/eventos/danza.jpg" height="300px" width="300%" 
+             alt="Club de danza" data-bs-toggle="modal" data-bs-target="#modaldanza">
+        <br><br>
+      </div>
+
+      <!-- Modal con Carrusel -->
+      <div class="modal fade" id="modaldanza" tabindex="-1" aria-labelledby="modaldanzaLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="modaldanzaLabel">Club de danza</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+            </div>
+            <div class="modal-body">
+              <!-- Carrusel -->
+              <div id="carouseldanza" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="public/image/eventos/danza1.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 1">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="public/image/eventos/danza2.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 2">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="public/image/eventos/danza3.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 3">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="public/image/eventos/danza4.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 4">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="public/image/eventos/danza5.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 5">
+                  </div>
+                </div>
+                <!-- Controles del Carrusel -->
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouseldanza" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Anterior</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouseldanza" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Siguiente</span>
+                </button>
+              </div>
+              <!-- Descripción del Evento -->
+              <p class="mt-3 text-center">
+              En la presentación de los cuadros de baile "Veracruz" y danzón.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+   <!-- Pie  -->
+<!-- Sección de  Destacada participación de nuestra institución en el "Concurso de escoltas" -->
+<div class="row mt-5">
+      <div class="col-md-6">
+        <h2><i class="bi bi-bullseye me-2"></i>Concurso de escoltas</h2>
+        <p style="text-align: justify;">Todo el reconocimiento para las participantes por el esfuerzo y la gallardía mostrada en las ejecuciones, a los padres de familia por su apoyo, a los profesores de educación física y a los que acompañaron a nuestra escolta en este evento.</p>
+
+
+        <!-- GALERÍA -->
+<section id="galeria">
+  <!-- 1era fila de imágenes -->
+  <div class="row">
+    <div class="col-md-4 zoomp">
       <img src="public/image/eventos/escol.jpg" height="300px" width="200%" alt="Concurso de escoltas" data-bs-toggle="modal" data-bs-target="#modalEscolta">
       <br><br>
     </div>
@@ -317,68 +445,62 @@ De la cual, se obtuvieron las siguientes posiciones:
 
 
 
- <!-- Sección de Eventos deportivos de Atletismo -->
+ <!-- Sección de Eventos club de danza  -->
   </div>
       <div class="col-md-6">
-      <h2><i class="bi bi-gear me-2"></i>Eventos deportivos de Atletismo</h2>
-      <p style="text-align: justify;">Destacada participación de nuestros jóvenes estudiantes en los Eventos deportivos de atletismo etapa supervisión num. 50.
-De la cual, se obtuvieron las siguientes posiciones:
-1er lugar 150 m. Varonil
-1er lugar 300 m. Varonil
-1er lugar 300 m. Femenil
-1er lugar 600 m. Femenil.</p>
+      <h2><i class="bi bi-gear me-2"></i>Club de danza </h2>
+      <p style="text-align: justify;">Participación del club de danza y alumnos de nuestra institución, en la presentación de los cuadros de baile "Veracruz" y danzón.</p>
       
  <!-- GALERÍA -->
  <section id="galeria">
     <div class="row">
       <div class="col-md-4 zoomp">
-        <img src="public/image/eventos/atle.jpg" height="350px" width="200%" 
-             alt="Eventos deportivos de Atletismo" data-bs-toggle="modal" data-bs-target="#modalAtletismo">
+        <img src="public/image/eventos/danza.jpg" height="350px" width="200%" 
+             alt="Club de danza" data-bs-toggle="modal" data-bs-target="#modaldanza">
         <br><br>
       </div>
 
       <!-- Modal con Carrusel -->
-      <div class="modal fade" id="modalAtletismo" tabindex="-1" aria-labelledby="modalAtletismoLabel" aria-hidden="true">
+      <div class="modal fade" id="modaldanza" tabindex="-1" aria-labelledby="modaldanzaLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="modalAtletismoLabel">Eventos deportivos de Atletismo</h5>
+              <h5 class="modal-title" id="modaldanzaLabel">Club de danza</h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
             </div>
             <div class="modal-body">
               <!-- Carrusel -->
-              <div id="carouselAtletismo" class="carousel slide" data-bs-ride="carousel">
+              <div id="carouseldanza" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="public/image/eventos/atle1.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 1">
+                    <img src="public/image/eventos/danza1.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 1">
                   </div>
                   <div class="carousel-item">
-                    <img src="public/image/eventos/atle2.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 2">
+                    <img src="public/image/eventos/danza2.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 2">
                   </div>
                   <div class="carousel-item">
-                    <img src="public/image/eventos/atle3.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 3">
+                    <img src="public/image/eventos/danza3.jpg" class="d-block mx-auto" style="width: 60%; height: auto;" alt="Imagen 3">
                   </div>
                   <div class="carousel-item">
-                    <img src="public/image/eventos/atle4.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 4">
+                    <img src="public/image/eventos/danza4.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 4">
                   </div>
                   <div class="carousel-item">
-                    <img src="public/image/eventos/atle5.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 5">
+                    <img src="public/image/eventos/danza5.jpg" class="d-block mx-auto" style="width: 60%; height: auto;"  alt="Imagen 5">
                   </div>
                 </div>
                 <!-- Controles del Carrusel -->
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselAtletismo" data-bs-slide="prev">
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouseldanza" data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                   <span class="visually-hidden">Anterior</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselAtletismo" data-bs-slide="next">
+                <button class="carousel-control-next" type="button" data-bs-target="#carouseldanza" data-bs-slide="next">
                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
                   <span class="visually-hidden">Siguiente</span>
                 </button>
               </div>
               <!-- Descripción del Evento -->
               <p class="mt-3 text-center">
-                Nos sentimos orgullosos de ustedes porque con su esfuerzo han dejado muy en alto el nombre de nuestra institución.
-              </p>
+              En la presentación de los cuadros de baile "Veracruz" y danzón.
             </div>
           </div>
         </div>
@@ -386,7 +508,8 @@ De la cual, se obtuvieron las siguientes posiciones:
     </div>
     </div>
 
- 
+
+
 
 
 
